70ecd8ec39e68607de0256e92ef6c674256eee2c
Syimyk Kairatbekovich <chatykulov2001@yandex.ru>
2022-03-17 04:38:50 +0300

    Task ts file

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from './task';
 
 @Component({
   selector: 'app-tasks',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
+  all_task: Task[] = [
+     { id: 1, name: 'Commit changes', isComplete:false },
+     { id: 2, name: 'Do my homework', isComplete:false },
+     { id: 3, name: 'Prepare presentation', isComplete:false },
+     { id: 4, name: 'Send email', isComplete:false },
+     { id: 5, name: 'Buy products', isComplete:false },
+     { id: 6, name: 'Lunch with Mom', isComplete:true },
+     { id: 7, name: 'Start learning Angular',  isComplete:false },
+     { id: 9, name: 'Feed my dog', isComplete:false }
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
